2987b3a0d1379c018e22796a16962080120cac87
Susie Douang <soutsada.d@gmail.com>
2024-08-30 08:36:47 -0500

    include React-Router and Routing

--- src/Pet.jsx
+++ src/Pet.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 const Pet = ({ name, animal, breed, images, location, id }) => {
   let hero = "http://pets-images.dev-apis.com/pets/none.jpg";
   if (images.length) {
@@ -5,7 +7,7 @@ const Pet = ({ name, animal, breed, images, location, id }) => {
   }
 
   return (
-    <a href={`/details/${id}`} className="pet" key={id}>
+    <Link to={`/details/${id}`} className="pet" key={id}>
       <div className="image-container">
         <img src={hero} alt={name} />
       </div>
@@ -15,7 +17,7 @@ const Pet = ({ name, animal, breed, images, location, id }) => {
           {animal} - {breed} - {location}
         </h2>
       </div>
-    </a>
+    </Link>
   );
 };
 
