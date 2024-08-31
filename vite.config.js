882f7f90d9fad0a85b5d7c96260752ae5a2a2f9b
Susie Douang <soutsada.d@gmail.com>
2024-08-30 19:00:10 -0500

    deployment configurations for GitHub Pages / Hosting

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
 import { defineConfig } from "vite";
 import react from '@vitejs/plugin-react';
+import path from "path";
+import { resolve } from "node:path";
 
 export default defineConfig({
     plugins: [react()],
+    base: "./",
     root: "src",
-})
+    build: {
+        outDir: "dist", // Output directory for production build.
+        assetsDir: "assets", // Directory for static assets in the build output.
+        minify: "esbuild", // Minification options (terser or esbuild).
+        resolve: {
+            alias: {
+                "@": path.resolve(__dirname, "./src"),
+            },
+        },
+    },
+},
+)
